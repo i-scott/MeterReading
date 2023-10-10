1797fbb1273961fd82bca53ba8a79f05035525d4
ScoRoScuba <scott.roberts@virgin.net>
2023-10-10 18:57:44 +0100

    Added in Reading Controller and removed Redundant WeatherForecastController

---
+++ MeterReading-WebAPI/Controllers/ReadingsController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using Swashbuckle.AspNetCore.Filters;
+using Microsoft.Net.Http.Headers;
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace Controllers {
+
+    [ApiController]
+    public class ReadingsController : ControllerBase
+    {
+        private readonly ILogger<ReadingsController> _logger;
+
+        public ReadingsController(ILogger<ReadingsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost()]
+        [MapToApiVersion("1.0")]
+        [Route("api/v{version:apiVersion}/meter-reading-uploads")]
+        [ProducesResponseType(typeof(string), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        [SwaggerResponseHeader(StatusCodes.Status201Created, "Location", "URI", "Location to the newly created catalogue item")]
+        [SwaggerOperation(Summary = "Upload Meter Reading Data in CSV Format",
+                            Description = "Uploads, validates and Creates Meter Reading Data",
+                            OperationId = "meter-reading-uploads")]
+        public IActionResult MeterReadingUpload([FromBody] string metaData)
+        {            
+            return Ok();            
+        }
+
+        [HttpPut()]
+        [MapToApiVersion("1.0")]
+        [Route("api/v{version:apiVersion}/meter-reading-uploads")]
+        [ProducesResponseType(typeof(string), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        [SwaggerResponseHeader(StatusCodes.Status201Created, "Location", "URI", "Location to the newly created catalogue item")]
+        [SwaggerOperation(Summary = "Large File Upload Meter Reading Data in CSV Format",
+                            Description = "Uploads, validates and Creates Meter Reading Data",
+                            OperationId = "meter-reading-uploads")]
+        public async Task<IActionResult> MeterReadingUploadAsync()
+        {
+            // Check if the request contains multipart/form-data.
+            var request = HttpContext.Request;
+
+            // validation of Content-Type
+            // 1. first, it must be a form-data request
+            // 2. a boundary should be found in the Content-Type
+            if (!request.HasFormContentType ||
+                !MediaTypeHeaderValue.TryParse(request.ContentType, out var mediaTypeHeader) ||
+                string.IsNullOrEmpty(mediaTypeHeader.Boundary.Value))
+            {
+                return new UnsupportedMediaTypeResult();
+            }
+
+            var boundary = HeaderUtilities.RemoveQuotes(mediaTypeHeader.Boundary.Value).Value;
+            var reader = new MultipartReader(boundary, request.Body);
+            var section = await reader.ReadNextSectionAsync();
+
+            // This sample try to get the first file from request and save it
+            // Make changes according to your needs in actual use
+            while (section != null)
+            {
+                var hasContentDispositionHeader = ContentDispositionHeaderValue.TryParse(section.ContentDisposition,
+                    out var contentDisposition);
+
+                if (hasContentDispositionHeader && contentDisposition.DispositionType.Equals("form-data") &&
+                    !string.IsNullOrEmpty(contentDisposition.FileName.Value))
+                {
+                    // Don't trust any file name, file extension, and file data from the request unless you trust them completely
+                    // Otherwise, it is very likely to cause problems such as virus uploading, disk filling, etc
+                    // In short, it is necessary to restrict and verify the upload
+                    // Here, we just use the temporary folder and a random file name
+
+                    // Get the temporary folder, and combine a random file name with it
+                    var fileName = Path.GetRandomFileName();
+                    var saveToPath = Path.Combine(Path.GetTempPath(), fileName);
+
+                    using (var targetStream = System.IO.File.Create(saveToPath))
+                    {
+                        await section.Body.CopyToAsync(targetStream);
+                    }
+
+                    return Ok();
+                }
+
+                section = await reader.ReadNextSectionAsync();
+            }
+
+            // If the code runs to this location, it means that no files have been saved
+            return BadRequest("No files data in the request.");
+        }
+    }
+}
